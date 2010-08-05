36a47de90e189fe3093015597b8d7a4402c9c5b7
santhosh <santhoshonet@gmail.com>
2010-08-05 19:30:04 +0530

    Bug Fixed

--- public/javascripts/Index.js
+++ public/javascripts/Index.js
@@ -41,7 +41,5 @@ $(function () {
 	{
 		return str.replace(/^\s+|\s+$/g, ''); 
 	};
-    setTimeout(function() {
-        window.location = "https://santech.createsend.com/login.aspx?username=" + $('#username').html() + "&password=" + $('#password').html();
-    },2000)
+   
 });
---
+++ public/javascripts/Success.js
@@ -0,0 +1,5 @@
+$(function() {
+setTimeout(function() {
+        window.location = "https://santech.createsend.com/login.aspx?username=" + $('#username').html() + "&password=" + $('#password').html();
+    },2000);
+}) 
